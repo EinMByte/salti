dcd311dce18e46734cd792022be14f7e20109e51
EinMByte <einmbyte@i2pmail.org>
2016-09-29 23:34:03 +0200

    Add essential extension files.

---
+++ chrome/content/menu.js
@@ -0,0 +1,16 @@
+var saltiMenu = {
+    showSettings : function() {
+        window.open(
+            "chrome://salti/content/options.xul",
+            "salti options",
+            "chrome"
+        );
+    },
+    showAbout : function() {
+        window.open(
+            "chrome://salti/content/about.xul",
+            "about",
+            "chrome"
+        ); 
+    },
+};
---
+++ defaults/preferences/salti.js
@@ -0,0 +1,3 @@
+// I2P proxy server host and port
+pref("extensions.salti.host", "127.0.0.1");
+pref("extensions.salti.port", 4444);
